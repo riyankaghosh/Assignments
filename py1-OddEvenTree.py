9168885179c2f9fae5c2b7830849653f8dbd2779
riyankaghosh <riyankaghosh8@gmail.com>
2017-08-23 12:25:23 +0530

    This is my third commit

---
+++ py1-OddEvenTree.py
@@ -0,0 +1,21 @@
+import itertools
+import random
+
+class OddEvenTree():
+    def getTree(self,x):
+        c=[]
+        y = itertools.combinations(range(0,len(x)),2)
+        for i in y:
+            if (x[i[0]][i[1]]==x[i[1]][i[0]]) and (x[i[0]][i[0]]=='E') :
+                for i in range(0, len(x)-1):
+                    y = i
+                    z = i+1
+                    c.append((y,z))       
+
+            elif (x[i[0]][i[1]]!=x[i[1]][i[0]]) or (x[i[0]][i[0]]!='E'):
+                return [-1]
+
+        m = random.sample(list(set(c)), len(x)-1)
+        n = itertools.chain.from_iterable(m)
+        return list(n)
+        
